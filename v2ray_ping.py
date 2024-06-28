43f721b9a90c494c3cee2ab24e272d84c7dd52b1
Yiling Cao <yiling.cao@gmail.com>
2024-06-29 02:07:19 +0800

    add report to host for remote pings.

--- v2ray_ping.py
+++ v2ray_ping.py
@@ -1,5 +1,7 @@
 from v2ray_speed_map import *
 import sys
+import datetime
+import requests
 
 temp_files_prefix = "/tmp/v2ray_config_"
 
@@ -19,6 +21,11 @@ def write_airports_to_file(airport_dicts, temp_files_prefix):
         with open(temp_files_prefix + str(i) +".json", 'w') as f:
             f.write(json.dumps(airport_dict))
 
+def report_pings_to_server(url, results_json):
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=json.dumps(results_json), headers=headers)
+    print("Status Code:", response.status_code)
+    print("Response Text:", response.text)
 
 if __name__ == "__main__":
     #check if argv empty
@@ -34,6 +41,8 @@ if __name__ == "__main__":
     print(airports)
     airport_dicts = airports_to_dicts(airports)
 
+    airport_names = [airport_dict["comments"] for airport_dict in airport_dicts]
+  
     kill_v2ray_processes_with_args_containing("v2ray_config_")
 
     write_airports_to_file(airport_dicts, temp_files_prefix)
@@ -42,9 +51,25 @@ if __name__ == "__main__":
 
     tcp_pings = test_tcp_pings(airport_dicts)
 
+    print(tcp_pings)
+
     print("Testing public ip...")
     public_ips=test_public_ip(airport_dicts)
+
+    print(public_ips)
 
     #test google pings
     print("Testing google pings...")
     google_pings = test_google_pings(airport_dicts)
+
+    print(google_pings)
+    hostname = socket.gethostname()
+    results = []
+    for i, airport_names in enumerate(airport_names):
+        ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+        results.append({"initator": hostname,"name": airport_names, "tcp_ping": tcp_pings[i], "google_ping": google_pings[i], "public_ip": public_ips[i], "created_at": ts})
+    
+    print(results)
+
+    report_pings_to_server(configs["post_remote_server_url"], results)
+    
